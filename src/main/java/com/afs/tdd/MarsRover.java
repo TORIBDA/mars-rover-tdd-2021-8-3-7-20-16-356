9aa7ec8d9da38af588d491ef5b5eb0654c1f86b2
TORIBDA <david.moses.toribio@oocl.com>
2021-08-05 21:55:34 +0800

    [MarsRover][TORIBDA] feat: added mixed commands

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -2,67 +2,62 @@ package com.afs.tdd;
 
 public class MarsRover {
     private RoverStatus roverStatus;
+
     public MarsRover(RoverStatus roverStatus) {
-        this.roverStatus=roverStatus;
+        this.roverStatus = roverStatus;
     }
 
-    public void executeCommand(String command) {
-        if (command.equals("M"))
-        {
+    public void executeCommands(String commands) {
+        for (Character command : commands.toCharArray()) {
+            executeCommand(command);
+        }
+    }
+
+    public void executeCommand(Character command) {
+        if (command.equals('M')) {
             move();
         }
-        if(command.equals("L"))
-        {
+        if (command.equals('L')) {
             turnLeft();
         }
-        if(command.equals("R"))
-        {
+        if (command.equals('R')) {
             turnRight();
         }
     }
 
     private void turnRight() {
-        if(roverStatus.getDirection().equals("N")){
+        if (roverStatus.getDirection().equals("N")) {
             roverStatus.setDirection("E");
-        }
-        else if(roverStatus.getDirection().equals("W")){
+        } else if (roverStatus.getDirection().equals("W")) {
             roverStatus.setDirection("N");
-        }
-        else if(roverStatus.getDirection().equals("E")){
+        } else if (roverStatus.getDirection().equals("E")) {
             roverStatus.setDirection("S");
-        }
-        else if(roverStatus.getDirection().equals("S")){
+        } else if (roverStatus.getDirection().equals("S")) {
             roverStatus.setDirection("W");
         }
     }
 
     private void turnLeft() {
-        if(roverStatus.getDirection().equals("N")){
+        if (roverStatus.getDirection().equals("N")) {
             roverStatus.setDirection("W");
-        }
-        else if(roverStatus.getDirection().equals("W")){
+        } else if (roverStatus.getDirection().equals("W")) {
             roverStatus.setDirection("S");
-        }
-        else if(roverStatus.getDirection().equals("E")){
+        } else if (roverStatus.getDirection().equals("E")) {
             roverStatus.setDirection("N");
-        }
-        else if(roverStatus.getDirection().equals("S")){
+        } else if (roverStatus.getDirection().equals("S")) {
             roverStatus.setDirection("E");
         }
     }
 
     private void move() {
-        if(roverStatus.getDirection().equals("N")) {
-            roverStatus.setLocationY(roverStatus.getLocationY()+1);
-        }
-        else if(roverStatus.getDirection().equals("W")) {
-            roverStatus.setLocationX(roverStatus.getLocationX()-1);
-        }
-        else if(roverStatus.getDirection().equals("E")) {
-            roverStatus.setLocationX(roverStatus.getLocationX()+1);
-        }
-        else if(roverStatus.getDirection().equals("S")) {
-            roverStatus.setLocationY(roverStatus.getLocationY()-1);
+        if (roverStatus.getDirection().equals("N")) {
+            roverStatus.setLocationY(roverStatus.getLocationY() + 1);
+        } else if (roverStatus.getDirection().equals("W")) {
+            roverStatus.setLocationX(roverStatus.getLocationX() - 1);
+        } else if (roverStatus.getDirection().equals("E")) {
+            roverStatus.setLocationX(roverStatus.getLocationX() + 1);
+        } else if (roverStatus.getDirection().equals("S")) {
+            roverStatus.setLocationY(roverStatus.getLocationY() - 1);
         }
     }
 
