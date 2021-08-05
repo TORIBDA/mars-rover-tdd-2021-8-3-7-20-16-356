d620b3ea8302253c0765dc5d92a9bcb4bd446ccd
TORIBDA <david.moses.toribio@oocl.com>
2021-08-05 20:45:14 +0800

    [MarsRover][TORIBDA] test: added test case for should_return_00N_when_execute_command_given_00W_R

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -69,4 +69,17 @@ class DemoTest {
         assertEquals(expectedRoverStatus.getLocationY(), marsRover.getRoverStatus().getLocationY());
         assertEquals(expectedRoverStatus.getDirection(), marsRover.getRoverStatus().getDirection());
     }
+
+    @Test
+    public void should_return_00N_when_execute_command_given_00W_R() {
+        //given
+        MarsRover marsRover = new MarsRover(new RoverStatus(0, 0, "W"));
+        RoverStatus expectedRoverStatus = new RoverStatus(0, 0, "N");
+        //when
+        marsRover.executeCommand("R");
+        //then
+        assertEquals(expectedRoverStatus.getLocationX(), marsRover.getRoverStatus().getLocationX());
+        assertEquals(expectedRoverStatus.getLocationY(), marsRover.getRoverStatus().getLocationY());
+        assertEquals(expectedRoverStatus.getDirection(), marsRover.getRoverStatus().getDirection());
+    }
 }
